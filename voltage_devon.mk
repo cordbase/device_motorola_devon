66931ec668c47a9d722950806d20cd7cb30e68b1
CordBase <144811064+cordbase@users.noreply.github.com>
2025-09-27 22:26:01 +0530

    Update and rename lineage_devon.mk to voltage_devon.mk

--- voltage_devon.mk
+++ voltage_devon.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/devon/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_devon
+PRODUCT_NAME := voltage_devon
 PRODUCT_DEVICE := devon
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g32
